1e7fae260b3523301f0f53d7e02cd0c4f389a271
Misha <Mzhebrunov@gmail.com>
2022-07-28 21:54:58 -0400

    Adds basic funcionality for verion 1.0.3.

--- src/asm.h
+++ src/asm.h
@@ -1,21 +1,60 @@
-void render_init(void);
+typedef enum
+{
+    GNU,
+    MASM
+} asm_enum;
+
+void render_init(asm_enum asm_e);
 
 void bss_add(char * item);
 void data_add(char * item);
 void text_add(char * item);
 
-void combine_code(void);
+void code_add(char * item);
+void data_add(char * item);
+void dataq_add(char * item);
+void stack_add(char * item);
+
+void combine_code(char * before);
 void export_to(char * path);
 
-extern char ** bss;
-extern char ** data;
-extern char ** text;
-extern char * code;
+extern unsigned short gnu_e;
 
-extern unsigned int bss_next;
-extern unsigned int data_next;
-extern unsigned int text_next;
+extern char * output;
 
-extern unsigned int bss_alloc;
-extern unsigned int data_alloc;
-extern unsigned int text_alloc;
+typedef struct gnu
+{
+    char ** bss;
+    char ** data;
+    char ** text;
+
+    unsigned long bss_next;
+    unsigned long data_next;
+    unsigned long text_next;
+
+    unsigned long bss_alloc;
+    unsigned long data_alloc;
+    unsigned long text_alloc;
+} gnu_t;
+
+typedef struct masm
+{
+    char ** code;
+    char ** data;
+    char ** dataq;
+    char ** stack;
+
+    unsigned long data_next;
+    unsigned long dataq_next;
+    unsigned long stack_next;
+    unsigned long code_next;
+
+    unsigned long data_alloc;
+    unsigned long dataq_alloc;
+    unsigned long stack_alloc;
+    unsigned long code_alloc;
+
+} masm_t;
+
+extern gnu_t gnu_i;
+extern masm_t masm_i;
